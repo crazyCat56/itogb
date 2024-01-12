d07ab854a7bb7d1bff331a95771795be86962c0b
unknown <kydimasik@gmail.com>
2024-01-12 23:54:14 +0300

    проверка + создания нового массива

--- Program.cs
+++ Program.cs
@@ -7,3 +7,30 @@ void array(string [] stringArray)
      stringArray[i] = Console.ReadLine()!;
   }
 }
+string [] symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  else 
+   {
+     Console.Write($"неккоретный ввод, число должно состоять из 3-х символов или мение)") ;
+     break ;
+    
+   }
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+
